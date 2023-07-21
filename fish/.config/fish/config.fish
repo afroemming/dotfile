0cc5c3c63c20f3ab43fae15ce96593fb57fbea42
Amelia Froemming <7919407+afroemming@users.noreply.github.com>
2023-07-21 10:26:40 -0500

    Add fish config files.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+function fish_greeting
+    echo Hello friend!
+    echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+end
